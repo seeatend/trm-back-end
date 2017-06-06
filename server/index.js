01cbaecd2ffbaf6e3dabfdf696e55201da5498ed
seeatend <readydone19@gmail.com>
2017-06-06 11:48:07 +0100

    Added logs utility.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
@@ -13,6 +14,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
+
+app.use(morgan('dev'))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
--- server/routes/message.js
+++ server/routes/message.js
@@ -10,13 +10,12 @@ const {extension} = require('utils/file')
 
 const mkdirp = require('mkdirp')
 
-
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: (req, file, cb) => {
     let destination = path.resolve('./uploads/tmp')
     mkdirp(destination, cb.bind(this, null, destination))
   },
-  filename: function(req, file, cb) {
+  filename: (req, file, cb) => {
     let fieldName = file.fieldname
     let fileName = `${fieldName}-${Date.now()}.${extension(file.originalname)}`
     cb(null, fileName)
