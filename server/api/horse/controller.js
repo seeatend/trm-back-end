b5b829ae7f6182208bb1c4f67346f1f283aa0759
seeatend <readydone19@gmail.com>
2017-06-13 14:27:55 +0100

    Added runs and wins to horse endpoint and removed unnecessary fields.

--- server/api/horse/controller.js
+++ server/api/horse/controller.js
@@ -1,8 +1,19 @@
-const mongoose = require('mongoose')
 const {Horse} = require('./model')
 const {prepareQuery, dehyphenize, success, error} = require('utils/request')
 
 const availableQueries = ['name']
+
+const prepareHorse = horse => {
+  horse.runs = horse.performances.length
+  horse.wins = horse.performances.filter(
+    p => { return p.position.official === 1}
+  ).length
+  const removeProps = ['performances', 'timeFormId']
+  removeProps.forEach(prop => {
+    delete horse[prop]
+  })
+  return horse
+}
 
 exports.getHorse = function (req, res) {
   let query = prepareQuery(req.query, availableQueries)
@@ -10,13 +21,9 @@ exports.getHorse = function (req, res) {
     query.name = dehyphenize(query.name)
     Horse.findOne(
       query,
-      {__v: false, _id: false},
-      {
-        limit: 20,
-        sort: {createdAt: -1}
-      }
+      {__v: false, _id: false}
     ).then(horse => {
-      res.json(success(horse))
+      res.json(success(prepareHorse(horse.toObject())))
     }).catch(err => {
       res.send(error(err))
     })
