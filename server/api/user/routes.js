170a660167761b11bd322963b7d074e548dcfd77
seeatend <readydone19@gmail.com>
2017-07-03 15:25:34 +0100

    Exposed /user/register and /user/login endpoints for login and register using passport.js.

--- server/api/user/routes.js
+++ server/api/user/routes.js
@@ -3,9 +3,13 @@ const express = require('express')
 const router = express.Router({mergeParams: true})
 
 const dashboardRoute = require('./dashboard/routes')
+const registerRoute = require('./register/routes')
+const loginRoute = require('./login/routes')
 
 const routePath = '/user'
 
 router.use(routePath, dashboardRoute)
+router.use(routePath, registerRoute)
+router.use(routePath, loginRoute)
 
 module.exports = router
