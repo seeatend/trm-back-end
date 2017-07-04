82b34ac04469c6f3fc6d2e2b24b30d051be3bf0b
seeatend <readydone19@gmail.com>
2017-07-04 13:52:12 +0100

    Added more horses to popualet script.

--- server/timeform/updateHorse.js
+++ server/timeform/updateHorse.js
@@ -39,110 +39,112 @@ const getRandomColor = () => {
 }
 
 module.exports = (horse, additionalData = {}) => {
-  console.log(`Processing: ${horse.horseName}`)
-  let horseData, syndicateData
+  if (horse) {
+    console.log(`Processing: ${horse.horseName}`)
+    let horseData, syndicateData
 
-  return new Promise((resolve, reject) => {
-    performances.get({
-      $filter: `horseCode eq '${horse.horseCode}'`
-    }).then(performances => {
-      console.log(`Performances for: ${horse.horseName}`)
+    return new Promise((resolve, reject) => {
+      performances.get({
+        $filter: `horseCode eq '${horse.horseCode}'`
+      }).then(performances => {
+        console.log(`Performances for: ${horse.horseName}`)
 
-      horseData = convert.horse(horse)
+        horseData = convert.horse(horse)
 
-      let performancesData = []
-      performances.forEach(performance => {
-        performancesData.push(convert.performance(performance))
-      })
-
-      horseData.performances = performancesData
-      horseData = Object.assign(horseData, additionalData)
-      horseData.name = horseData.name.toUpperCase()
-
-      if (!horseData.owner || !horseData.owner.name) {
-        reject(`Horse owner is undefined(${horseData.name})`)
-      }
-
-      let syndicateFiles = []
-      let syndicateData = {}
-      if (horseData.syndicate) {
-        horseData.owner.name = horseData.syndicate.name || horseData.owner.name
-        syndicateData = horseData.syndicate
-        if (syndicateData.featuredImage) {
-          syndicateFiles.push(mockFileUpload(
-            'featuredImage', syndicateData.featuredImage
-          ))
-        }
-        if (syndicateData.logo) {
-          syndicateFiles.push(mockFileUpload(
-            'logo', syndicateData.logo
-          ))
-        }
-      }
-      horseData.owner.name = horseData.owner.name.toUpperCase()
-
-      return updateSyndicate(
-        Object.assign(
-          {
-            color: colors.pop() || getRandomColor(),
-            owner: horseData.owner
-          },
-          syndicateData
-        ),
-        syndicateFiles
-      )
-    }).then(syndicate => {
-      console.log('Updated syndicate')
-      syndicateData = syndicate
-      horseData.owner._id = syndicate._id
-      horseData.owner.color = syndicate.color
-      horseData.racingType = Math.random() > .6 ? 'National Hunt' : Math.random() > .3 ? 'Flat Racing' : 'Dual Purpose'
-      horseData.ownership = {
-        type: Math.random() > .5 ? 'Fixed Period' : 'Open Ended Period'
-      }
-      if (horseData.ownership.type === 'Open Ended Period') {
-        horseData.ownership.years = randomInteger(1, 6)
-      }
-      horseData.cost = {
-        monthly: randomInteger(500, 1500) * 5,
-        initial: randomInteger(2100, 4500) * 5
-      }
-      let timeformId = horse.horseCode.trim()
-      let horseFiles = []
-      if (additionalData.img) {
-        horseFiles.push(mockFileUpload(
-          'featuredImage', additionalData.img
-        ))
-      }
-      if (additionalData.thumbnail) {
-        horseFiles.push(mockFileUpload(
-          'thumbnailImage', additionalData.thumbnail
-        ))
-      }
-
-      return updateHorse(
-        {timeformId},
-        horseData,
-        horseFiles
-      )
-    }).then(savedHorse => {
-      console.log(`Saved: ${horse.horseName}`)
-
-      if (savedHorse._id && syndicateData.horses.indexOf(savedHorse._id) < 0) {
-        syndicateData.horses.push(savedHorse._id)
-        return syndicateData.save().then(res => {
-          console.log('Horse added to syndicate')
-          resolve()
-        }).catch(err => {
-          reject(err)
+        let performancesData = []
+        performances.forEach(performance => {
+          performancesData.push(convert.performance(performance))
         })
-      }
-      else {
-        console.log('Horse already part of syndicate')
-        resolve()
-      }
-    }).catch(err => {
-      reject(err)
+
+        horseData.performances = performancesData
+        horseData = Object.assign(horseData, additionalData)
+        horseData.name = horseData.name.toUpperCase()
+
+        if (!horseData.owner || !horseData.owner.name) {
+          reject(`Horse owner is undefined(${horseData.name})`)
+        }
+
+        let syndicateFiles = []
+        let syndicateData = {}
+        if (horseData.syndicate) {
+          horseData.owner.name = horseData.syndicate.name || horseData.owner.name
+          syndicateData = horseData.syndicate
+          if (syndicateData.featuredImage) {
+            syndicateFiles.push(mockFileUpload(
+              'featuredImage', syndicateData.featuredImage
+            ))
+          }
+          if (syndicateData.logo) {
+            syndicateFiles.push(mockFileUpload(
+              'logo', syndicateData.logo
+            ))
+          }
+        }
+        horseData.owner.name = horseData.owner.name.toUpperCase()
+
+        return updateSyndicate(
+          Object.assign(
+            {
+              color: colors.pop() || getRandomColor(),
+              owner: horseData.owner
+            },
+            syndicateData
+          ),
+          syndicateFiles
+        )
+      }).then(syndicate => {
+        console.log('Updated syndicate')
+        syndicateData = syndicate
+        horseData.owner._id = syndicate._id
+        horseData.owner.color = syndicate.color
+        horseData.racingType = Math.random() > .6 ? 'National Hunt' : Math.random() > .3 ? 'Flat Racing' : 'Dual Purpose'
+        horseData.ownership = {
+          type: Math.random() > .5 ? 'Fixed Period' : 'Open Ended Period'
+        }
+        if (horseData.ownership.type === 'Open Ended Period') {
+          horseData.ownership.years = randomInteger(1, 6)
+        }
+        horseData.cost = {
+          monthly: randomInteger(500, 1500) * 5,
+          initial: randomInteger(2100, 4500) * 5
+        }
+        let timeformId = horse.horseCode.trim()
+        let horseFiles = []
+        if (additionalData.img) {
+          horseFiles.push(mockFileUpload(
+            'featuredImage', additionalData.img
+          ))
+        }
+        if (additionalData.thumbnail) {
+          horseFiles.push(mockFileUpload(
+            'thumbnailImage', additionalData.thumbnail
+          ))
+        }
+
+        return updateHorse(
+          {timeformId},
+          horseData,
+          horseFiles
+        )
+      }).then(savedHorse => {
+        console.log(`Saved: ${horse.horseName}`)
+
+        if (savedHorse._id && syndicateData.horses.indexOf(savedHorse._id) < 0) {
+          syndicateData.horses.push(savedHorse._id)
+          return syndicateData.save().then(res => {
+            console.log('Horse added to syndicate')
+            resolve()
+          }).catch(err => {
+            reject(err)
+          })
+        }
+        else {
+          console.log('Horse already part of syndicate')
+          resolve()
+        }
+      }).catch(err => {
+        reject(err)
+      })
     })
-  })
+  }
 }
