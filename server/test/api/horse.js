a62e178b287060ff21d0ab980fc3a50fc73d949f
seeatend <readydone19@gmail.com>
2017-07-19 13:51:14 +0100

    Completed updateOrCreateHorse tests.

--- server/test/api/horse.js
+++ server/test/api/horse.js
@@ -5,7 +5,7 @@ const {expect} = require('chai')
 const {updateOrCreateHorse} = require('api/horse/controller')
 
 const requiredProps = {
-  name: 'To be Nick'
+  name: 'To Be Nick'
 }
 
 describe('Horses', () => {
@@ -14,13 +14,34 @@ describe('Horses', () => {
       done()
     })
   })
-  describe('/update horse', () => {
+  describe('/updateOrCreateHorse', () => {
     it('it should create horse', (done) => {
       updateOrCreateHorse(
         requiredProps
       ).then(res => {
         expect(res).to.exist
         expect(res.name).to.eql(requiredProps.name)
+        done()
+      })
+    })
+
+    it('it should update horse', (done) => {
+      let additionalData = {
+        trainer: {
+          name: 'Hege'
+        }
+      }
+      updateOrCreateHorse(
+        requiredProps
+      ).then(horse => {
+        return updateOrCreateHorse(
+          {name: horse.name},
+          additionalData
+        )
+      }).then(horse => {
+        expect(horse).to.exist
+        expect(horse.name).to.eql(requiredProps.name)
+        expect(horse.trainer.name).to.eql(additionalData.trainer.name)
         done()
       })
     })
