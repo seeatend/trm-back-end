114cddb2cdaf7d3b6ea9e30a5cb112cc8bdddf59
seeatend <readydone19@gmail.com>
2017-07-19 12:07:46 +0100

    Removed old api-tests.

---
+++ server/test/api/message.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+const Message = require('api/message/model')
+
+const {expect} = require('chai')
+
+describe('Messages', () => {
+  beforeEach((done) => {
+    Message.remove({}, () => {
+      done()
+    })
+  })
+  describe('/GET message', () => {
+    it('it should GET all the messages', (done) => {
+      expect(true).to.eql(true)
+      done()
+    })
+  })
+})
