f602565839d721f4439c35a71c4163434942babf
seeatend <readydone19@gmail.com>
2017-07-27 17:16:30 +0100

    Introduced email templating using handlebars.

--- server/utils/email/index.js
+++ server/utils/email/index.js
@@ -1,3 +1,8 @@
+const loadTemplate = require('./loadTemplate')
+const templates = {
+  verification: loadTemplate('verification')
+}
+
 const nodemailer = require('nodemailer')
 const mailgunTransport = require('nodemailer-mailgun-transport')
 
@@ -10,7 +15,13 @@ const auth = {
 
 const mailgun = nodemailer.createTransport(mailgunTransport(auth))
 
-const sendMail = data => {
+const sendMail = _data => {
+  let data = Object.assign({}, _data)
+  if (data.template) {
+    let template = templates[data.template.name]
+    data.html = template(data.template.data)
+    delete data.template
+  }
   return new Promise((resolve, reject) => {
     mailgun.sendMail(data, (err, info) => {
       if (err) {
