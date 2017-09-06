0c9068bd4b9386c6edfae91c3298a88caecd1247
Ali Azam <ali.azam@kcl.ac.uk>
2017-09-06 17:39:54 +0100

    Fixed a mistake

--- server/api/syndicate/controller/updateByName.js
+++ server/api/syndicate/controller/updateByName.js
@@ -1,7 +1,7 @@
 module.exports = function(_data) {
     let data = Object.assign({}, _data)
     let {syndicateName} = data
-    delete data.horseName
+    delete data.syndicateName
     if (!(syndicateName && syndicateName.length > 0) || Object.keys(data).length === 0) return Promise.reject()
     return this.updateOne({
         query: {
